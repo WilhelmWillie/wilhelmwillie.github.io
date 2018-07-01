5788da6c27f1cbe685254d4ba57cc5f5887402f2
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-06-30 22:10:02 -0700

    Rewriting my portfolio/personal site

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,23 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
-    less: {
-      development: {
+    sass: {
+      dist: {
         options: {
-          compress: true
+          style: 'compressed'
         },
         files: {
-          'assets/css/stylesheet.css': 'less/stylesheet.less'
+          'assets/css/stylesheet.css': 'scss/stylesheet.scss'
         }
       }
     },
     watch: {
-      files: ['**/*.less'],
-      tasks: ['less']
+      files: ['**/*.scss'],
+      tasks: ['sass']
     }
-  });
+  })
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-watch')
 
-  grunt.registerTask('default', ['less']);
-};
+  grunt.registerTask('default', ['watch'])
+}
