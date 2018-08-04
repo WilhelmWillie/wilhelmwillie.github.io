9ea432c4ba3b7f4957469c2d55db21b256b8df4b
Wilhelm Willie <wwillie@usc.edu>
2018-08-04 16:25:16 -0700

    Modified click to scroll div and added basic scroll functionality

--- assets/js/willie.js
+++ assets/js/willie.js
@@ -67,7 +67,7 @@ function updateSubtitle() {
         setTimeout(updateSubtitle, 75)
       }
     } else {
-      // Makes sure that the new subtitle text is unique 
+      // Makes sure that the new subtitle text is unique
       var newSubtitleTextIndex = subtitleTextIndex
 
       while (newSubtitleTextIndex === subtitleTextIndex) {
@@ -85,7 +85,7 @@ function updateSubtitle() {
     if (text !== newText) {
       // Add characters back to our subtitle
       if (text === '|') {
-        heroSubtitle.innerHTML = newText[0]       
+        heroSubtitle.innerHTML = newText[0]
       } else {
         heroSubtitle.innerHTML = newText.substring(0, text.length + 1)
       }
@@ -100,3 +100,29 @@ function updateSubtitle() {
 }
 
 setTimeout(updateSubtitle, 1500)
+
+// Testing this
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    targetY += 15;
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 10);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
