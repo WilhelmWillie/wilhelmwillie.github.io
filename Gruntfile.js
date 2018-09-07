5e360b043d45c79b4700b89f3b118ac6488f8a54
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-09-07 15:41:10 -0700

    Added uglify to grunt to minify javascript, added prod grunt task that aggregates production tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,12 +23,21 @@ module.exports = function (grunt) {
           dest: 'assets/img'
         }]
       }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'assets/js/willie.min.js': ['js/willie.js']
+        }
+      }
     }
   })
 
   grunt.loadNpmTasks('grunt-contrib-sass')
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin')
   grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
 
   grunt.registerTask('default', ['watch'])
+  grunt.registerTask('prod', ['sass', 'imagemin', 'uglify'])
 }
