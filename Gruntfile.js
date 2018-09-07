7cf28c1d707d33a37bae83f6cdd57a34fdf2edc7
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-09-07 15:30:08 -0700

    Added imd image minifier grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,21 @@ module.exports = function (grunt) {
     watch: {
       files: ['**/*.scss'],
       tasks: ['sass']
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'img/',
+          src: ['**/*.{png,jpg}'],
+          dest: 'assets/img'
+        }]
+      }
     }
   })
 
   grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch')
 
   grunt.registerTask('default', ['watch'])
